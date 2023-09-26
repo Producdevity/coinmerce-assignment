13af53f80377c8888a46d787576d648e2586dc48
Yassine Gherbi <ygherbi@anwb.nl>
2023-09-26 22:59:52 +0200

    chore: setup initial libraries and configurations

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,4 @@
-import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction } from "@remix-run/node"
 import {
   Links,
   LiveReload,
@@ -7,11 +6,13 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from "@remix-run/react"
+
+import stylesheet from "~/tailwind.css"
 
 export const links: LinksFunction = () => [
-  ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
-];
+  { rel: "stylesheet", href: stylesheet },
+]
 
 export default function App() {
   return (
@@ -21,6 +22,7 @@ export default function App() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
+        <title>Coinmerce - Cryptocurrency Price Tracking</title>
       </head>
       <body>
         <Outlet />
@@ -29,5 +31,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,15 +1,18 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/node"
 
 export const meta: MetaFunction = () => {
   return [
     { title: "New Remix App" },
     { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+  ]
+}
 
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1 className="text-3xl font-bold underline border-2">
+        Hello world! After TailwindCSS
+      </h1>
       <h1>Welcome to Remix</h1>
       <ul>
         <li>
@@ -37,5 +40,5 @@ export default function Index() {
         </li>
       </ul>
     </div>
-  );
+  )
 }
