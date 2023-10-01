cb669312b4cb6cc84d1f8cef54f46d0e4ecc6150
Yassine Gherbi <ygherbi@anwb.nl>
2023-10-01 19:06:47 +0200

    feat: add favicons. Overkill? 🧐

--- app/root.tsx
+++ app/root.tsx
@@ -15,6 +15,25 @@ import styles from '~/styles/tailwind.css'
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
   { rel: 'stylesheet', href: styles },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '180x180',
+    href: '/favicons/apple-touch-icon.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: '/favicons/favicon-32x32.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '16x16',
+    href: '/favicons/favicon-16x16.png',
+  },
+
+  { rel: 'manifest', href: '/site.webmanifest' },
 ]
 
 export default function App() {
