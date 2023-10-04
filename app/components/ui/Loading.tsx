b328c311c71eed0068ef91db6dfc335b36ac1539
Yassine Gherbi <ygherbi@anwb.nl>
2023-10-04 03:09:21 +0200

    chore: optional size prop

--- app/components/ui/Loading.tsx
+++ app/components/ui/Loading.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-function Loading() {
+interface Props {
+  size?: number
+}
+function Loading(props: Props) {
+  const size = props.size || 32
   return (
     <div className="flex h-full items-center justify-center">
-      <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-gray-900"></div>
+      <div
+        className={`h-${size} w-${size} animate-spin rounded-full border-b-2 border-t-2 border-gray-900`}
+      />
     </div>
   )
 }
