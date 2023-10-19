a48c901cb7c761e853c0c53a64d26c666f3ccf76
Yassine Gherbi <yassine@hexelnet.com>
2023-10-19 22:03:05 +0200

    fix: work around for hydration bug

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -7,11 +7,60 @@ import { RemixBrowser } from '@remix-run/react'
 import { startTransition, StrictMode } from 'react'
 import { hydrateRoot } from 'react-dom/client'
 
-startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>,
-  )
-})
+function clearBrowserExtensionInjectionsBeforeHydration() {
+  document
+    .querySelectorAll(
+      [
+        'html > *:not(body, head)',
+        'script[src*="extension://"]',
+        'link[href*="extension://"]',
+      ].join(', '),
+    )
+    .forEach((s) => {
+      s.parentNode?.removeChild(s)
+    })
+
+  const $targets = {
+    html: {
+      $elm: document.querySelector('html')!,
+      allowedAttributes: ['lang', 'dir', 'class'],
+    },
+    head: {
+      $elm: document.querySelector('head')!,
+      allowedAttributes: [''],
+    },
+    body: {
+      $elm: document.querySelector('body')!,
+      allowedAttributes: ['class'],
+    },
+  }
+
+  Object.entries($targets).forEach(([_, target]) => {
+    target.$elm.getAttributeNames().forEach((attr) => {
+      if (!target.allowedAttributes.includes(attr)) {
+        target.$elm.removeAttribute(attr)
+      }
+    })
+  })
+}
+
+function hydrate() {
+  clearBrowserExtensionInjectionsBeforeHydration()
+
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <StrictMode>
+        <RemixBrowser />
+      </StrictMode>,
+    )
+  })
+}
+
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate)
+} else {
+  // Sa5374 6506 0085 8616fari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  window.setTimeout(hydrate, 1)
+}
