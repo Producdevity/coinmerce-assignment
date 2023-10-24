732c00c91f7bbb68c7177ecdf146e5d95538db96
Yassine Gherbi <yassine@hexelnet.com>
2023-10-24 20:57:54 +0200

    feat: added animated underline. not really how I want to because of the rerenders after a page request. Will see if I can fix that later without delaying the page transition

--- app/components/Layout/Header.tsx
+++ app/components/Layout/Header.tsx
@@ -1,13 +1,50 @@
 import { Link, NavLink } from '@remix-run/react'
-import { useState } from 'react'
+import { useEffect, useRef, useState, type MouseEvent } from 'react'
 import CoinmerceLogo from '~/assets/images/coinmerce-logo.svg'
 import MenuIcon from '~/components/Icons/MenuIcon'
 import navLinks from '~/data/navLinks'
 import routes from '~/data/routes'
 import t from '~/utils/t'
 
+type UnderlineStyle = {
+  width: string
+  transform: string
+}
+
 function Header() {
   const [isNavOpen, setIsNavOpen] = useState(false)
+  const [underlineStyle, setUnderlineStyle] = useState<UnderlineStyle>({
+    width: '0',
+    transform: 'translateX(0px)',
+  })
+  const navRef = useRef<HTMLDivElement>(null)
+  const navLinkRefs = useRef<(HTMLAnchorElement | null)[]>([])
+
+  navLinkRefs.current = navLinks.map(
+    (_, index) => navLinkRefs.current[index] ?? null,
+  )
+
+  const updateUnderline = (ev: MouseEvent<HTMLAnchorElement>) => {
+    const navLeft = navRef.current
+      ? navRef.current.getBoundingClientRect().left
+      : 0
+    const rect = ev.currentTarget.getBoundingClientRect()
+    setUnderlineStyle({
+      width: `${rect.width}px`,
+      transform: `translateX(${rect.x - navLeft - 15}px)`,
+    })
+  }
+
+  useEffect(() => {
+    const activeRef = navLinkRefs.current.find(
+      (ref) => ref?.classList.contains('text-blue-500'),
+    )
+    if (!activeRef) return
+
+    updateUnderline({
+      currentTarget: activeRef,
+    } as MouseEvent<HTMLAnchorElement>)
+  }, [isNavOpen])
 
   return (
     <nav className="flex flex-col bg-slate-950 py-2 max-md:max-w-full">
@@ -31,24 +68,25 @@ function Header() {
           <span className="sr-only">{t('common.openMainMenu')}</span>
           <MenuIcon />
         </button>
-
-        {/* Spacer */}
         <div className="flex-grow max-md:hidden"></div>
 
         <div
           id="navbar-main"
+          ref={navRef}
           onClick={() => setIsNavOpen((prevState) => !prevState)}
-          className={`md:px-auto w-full flex-grow px-4 md:block md:w-auto ${
+          className={`md:px-auto relative w-full flex-grow px-4 md:block md:w-auto ${
             isNavOpen ? '' : 'hidden'
           }`}
         >
           <ul className="mt-0.5 flex max-w-full flex-grow items-start justify-between gap-4 self-center max-md:flex-wrap max-md:justify-center max-sm:flex-wrap">
-            {navLinks.map((navLink) => (
+            {navLinks.map((navLink, index) => (
               <li key={navLink.to}>
                 <NavLink
                   to={navLink.to}
+                  ref={(ref) => (navLinkRefs.current[index] = ref)}
+                  onClick={updateUnderline}
                   className={({ isActive }) =>
-                    `block rounded py-2 pl-3 pr-4 font-sans slashed-zero md:bg-transparent md:p-0 ${
+                    `block rounded py-2 pl-3 pr-4  font-sans slashed-zero md:bg-transparent md:p-0 ${
                       isActive ? 'text-blue-500' : 'text-white'
                     }`
                   }
@@ -58,9 +96,11 @@ function Header() {
               </li>
             ))}
           </ul>
+          <div
+            className="absolute bottom-0 h-0.5 bg-blue-500"
+            style={{ ...underlineStyle, transition: 'all 0.3s ease' }}
+          />
         </div>
-
-        {/* Spacer */}
         <div className="w-[150px] flex-grow max-md:hidden"></div>
       </div>
     </nav>
